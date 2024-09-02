b25e719e9b9451f754a21dc29c3c251c140cb5e9
Mikel Rojas <mikelarr.2004@gmail.com>
2024-09-01 23:34:56 -0600

    Start to develop structure

--- src/pages/Principal.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Principal: React.FC<{}> = () => {
-  return (
-    <div>This is Principal</div>
-  )
-}
-
-export default Principal;
---
+++ src/pages/Principal/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles.css'
+
+const Principal: React.FC<{}> = () => {
+  return (
+    <div className='container-fluid'>
+        <h1>Muliples servicios al alcance de su mano</h1>
+    
+    </div>
+  )
+}
+
+export default Principal;
